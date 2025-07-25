eb8a80c6f8b58eac13f2c7884d44f9c988a31a5a
telavip <megateca@outlook.com>
2025-07-25 16:03:39 -0300

    Add files via upload

---
+++ 259909.js
@@ -0,0 +1,8 @@
+    const specialLinks = {
+        "mx": { "259909/1/2": "https://mixdrop.my/e/l7w8v9jzcd6l99" },
+        "fm": { "259909/1/2": "https://filemoon.in/e/9nl2k0nykrmo" },
+        "st": { "259909/1/2": "https://vide0.net/e/y2qlj7jet3kb" },
+        "mx_leg": { "259909/1/2": "https://vide0.net/e/y2qlj7jet3kb" },
+        "fm_leg": { "259909/1/2": "https://vide0.net/e/y2qlj7jet3kb" },
+        "st_leg": { "259909/1/2": "https://vide0.net/e/y2qlj7jet3kb" }
+    };
